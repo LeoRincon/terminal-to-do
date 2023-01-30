3193ac68c4e9f55d9e87544c29c4bf7ef07aeab8
Leonardo Rincon <jleobotache@gmail.com>
2023-01-30 11:04:58 -0500

    feat: Create the menu to show the user

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 require('colors');
+
+const { showMenu, stopMessage } = require('./helpers/messages');
+
 console.clear();
 
 const main = async () => {
- console.log('hola mundo'.rainbow);
+ let opt = '';
+
+ do {
+  opt = await showMenu();
+  if (opt !== '0') await stopMessage();
+ } while (opt !== '0');
+
+ //  stopMessage();
 };
 
 main();
