6d0883eb45d2544966419e03008578011754f044
Leonardo Rincon <jleobotache@gmail.com>
2023-02-11 14:35:48 -0500

    add functions for create task and list task

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 require('colors');
 
-const { inquirerMenu, StopMenu } = require('./helpers/inquirer');
+const { inquirerMenu, StopMenu, readInput } = require('./helpers/inquirer');
+
 const Tasks = require('./models/tasks');
 
 const main = async () => {
  let selectedOptionUser = '';
 
+ const tasks = new Tasks();
+
  do {
   selectedOptionUser = await inquirerMenu();
-  console.log({ selectedOptionUser });
+
+  switch (selectedOptionUser) {
+   case '1':
+    const taskDescription = await readInput(
+     `${'New task description: '.blue}: `
+    );
+    tasks.createTask(taskDescription);
+    break;
+   case '2':
+    console.log(tasks._list);
+    break;
+   default:
+    break;
+  }
 
   await StopMenu();
  } while (selectedOptionUser !== '0');
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -18,7 +18,30 @@ const StopMenu = async () => {
  await inquirer.prompt(stopOptions);
 };
 
+const readInput = async (description) => {
+ const inputCreateTask = [
+  {
+   type: 'input',
+   name: 'description',
+   message: description,
+   validate(value) {
+    if (value.length === 0) {
+     return 'Please enter a value';
+    }
+    return true;
+   },
+  },
+ ];
+
+ const { description: descriptionTask } = await inquirer.prompt(
+  inputCreateTask
+ );
+
+ return descriptionTask;
+};
+
 module.exports = {
  inquirerMenu,
  StopMenu,
+ readInput,
 };
